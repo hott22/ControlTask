dfcb32ce06996ba51239b7ac31a00411c5d8b31f
Bobby <mif210210@gmail.com>
2022-01-24 22:34:49 +0300

    edit program

--- Program.cs
+++ Program.cs
@@ -2,32 +2,50 @@
 //формирует массив из строк, длина которых меньше или равна 3 символам. 
 //Первоначальный массив можно ввести с клавиатуры, лиюо задать на старте выполнения алгоритма.
 
+void CreateArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+}
 
-string [] array = {"1234", "qwe", "asdfg", "45", "end" };
+string[] AlteredArray(string[] array, int argument)
+{
 
-string [] AlteredArray (string [] array, int argument) 
-{ 
-   
     int size = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= argument )
+        if (array[i].Length <= argument)
         {
             size = size + 1;
         }
     }
-    string [] secondArray = new string[size];
-    int index =0;
+    string[] secondArray = new string[size];
+    int index = 0;
     for (int i = 0; i < array.Length; i++)
     {
 
-        if (array[i].Length <= argument )
+        if (array[i].Length <= argument)
         {
             secondArray[index] = array[i];
             index++;
         }
     }
     return secondArray;
-} 
+}
 
-string [] secondArray = AlteredArray (array, 3);
+// void PrintArray(string[] array)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]}, ");
+//     }
+//     Console.Write("]");
+// }
+
+string[] array = new string[10];
+CreateArray(array);
+string[] secondArray = AlteredArray(array, 3);
+//PrintArray(secondArray);
